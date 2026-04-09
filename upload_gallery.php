827bd41cac2aeba9afc720381e40fb81e576fc6c
tarekabdelrahman95-blip <tarekabdelrahman95@gmail.com>
2026-04-09 15:24:13 +0200

    sd

--- upload_gallery.php
+++ upload_gallery.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once 'config-live.php';
 
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_admin']) || !$_SESSION['is_admin']) {
     header("Location: index.php");
